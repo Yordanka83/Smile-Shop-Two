fcfcb4ca39253a3187b2a2a863fd78d1fad4a914
Yordanka Daskalova <joivdas83@abv.bg>
2021-04-17 19:55:51 +0300

    Create CategoryAddBindingModel.java

---
+++ SmileShop/src/main/java/softuni/smileShop/model/binding/CategoryAddBindingModel.java
@@ -0,0 +1,21 @@
+package softuni.smileShop.model.binding;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class CategoryAddBindingModel {
+    private String name;
+
+    public CategoryAddBindingModel() {
+    }
+
+    @NotBlank(message = "Name cannot be empty str")
+    @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
