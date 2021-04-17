9dc48b16b6cbc3f508983ac394fc010970c87a1b
Yordanka Daskalova <joivdas83@abv.bg>
2021-04-17 19:58:02 +0300

    Create ProductAllViewModel.java

---
+++ SmileShop/src/main/java/softuni/smileShop/model/view/ProductAllViewModel.java
@@ -0,0 +1,67 @@
+package softuni.smileShop.model.view;
+
+
+import java.math.BigDecimal;
+
+
+public class ProductAllViewModel {
+
+    private String id;
+    private String name;
+    private BigDecimal price;
+    private String imageUrl;
+    private String description;
+    private String category;
+
+
+    public ProductAllViewModel() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
